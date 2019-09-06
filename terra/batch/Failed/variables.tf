1f4e1a626d1b97b5ff018380d967e6d58d474004
tonybutzer <tonybutzer@gmail.com>
2019-09-06 05:42:09 -0400

    automatic git update from Makefile

---
+++ terra/batch/Failed/variables.tf
@@ -0,0 +1,10 @@
+variable "region" {
+  description = "The AWS region."
+  default = "us-west-2"
+}
+
+variable "sample" {
+  description = "the general name for the tf group"
+  default = "usgs-batch"
+}
+
--- terra/batch/batch.tf
+++ terra/batch/batch.tf
@@ -1,5 +1,29 @@
+## Make sure your Subnet has internet access
+
+## Just use the default vpc and subnet - they have internet access
+variable "subnet" {
+  default = "subnet-1bc2ad40"
+}
+
+variable "vpc" {
+  default = "vpc-ce78f4a8"
+}
+
+
+provider "aws" {
+  region     = "us-west-2"
+}
+
+data "aws_vpc" "sample" {
+  id = "${var.vpc}"
+}
+
+data "aws_subnet" "sample" {
+  id = "${var.subnet}"
+}
+
 resource "aws_iam_role" "ecs_instance_role" {
-  name = "ecs_instance_role"
+  name = "t_ecs_instance_role"
 
   assume_role_policy = <<EOF
 {
@@ -17,18 +41,19 @@ resource "aws_iam_role" "ecs_instance_role" {
 EOF
 }
 
+## This policy is CRITICAL - without this no container service and therefor things get stuck
 resource "aws_iam_role_policy_attachment" "ecs_instance_role" {
   role       = "${aws_iam_role.ecs_instance_role.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
 
 resource "aws_iam_instance_profile" "ecs_instance_role" {
-  name = "ecs_instance_role"
+  name = "t_ecs_instance_role"
   role = "${aws_iam_role.ecs_instance_role.name}"
 }
 
 resource "aws_iam_role" "aws_batch_service_role" {
-  name = "aws_batch_service_role"
+  name = "t_aws_batch_service_role"
 
   assume_role_policy = <<EOF
 {
@@ -52,37 +77,41 @@ resource "aws_iam_role_policy_attachment" "aws_batch_service_role" {
 }
 
 resource "aws_security_group" "sample" {
-  name = "aws_batch_compute_environment_security_group"
-}
-
-resource "aws_vpc" "sample" {
-  cidr_block = "10.1.0.0/16"
-}
-
-resource "aws_subnet" "sample" {
-  vpc_id     = "${aws_vpc.sample.id}"
-  cidr_block = "10.1.1.0/24"
+  name = "t_aws_batch_compute_environment_security_group"
+   egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
 }
 
 resource "aws_batch_compute_environment" "sample" {
-  compute_environment_name = "sample"
+  compute_environment_name = "t_sample"
 
   compute_resources {
     instance_role = "${aws_iam_instance_profile.ecs_instance_role.arn}"
 
     instance_type = [
-      "c4.large",
+      "optimal",
     ]
 
-    max_vcpus = 16
+    max_vcpus = 24
+    # min_vcpus = 4
+    # desired_vcpus = 4
     min_vcpus = 0
+    desired_vcpus = 0
 
     security_group_ids = [
       "${aws_security_group.sample.id}",
     ]
+    
+    ec2_key_pair = "bucketlauncher2-keypair"
 
     subnets = [
-      "${aws_subnet.sample.id}",
+      "${data.aws_subnet.sample.id}",
     ]
 
     type = "EC2"
@@ -93,4 +122,67 @@ resource "aws_batch_compute_environment" "sample" {
   depends_on   = ["aws_iam_role_policy_attachment.aws_batch_service_role"]
 }
 
+resource "aws_batch_job_queue" "this" {
+  name = "t_job-queue"
+  state = "ENABLED"
+  priority = "1"
+  compute_environments = [
+    "${aws_batch_compute_environment.sample.arn}",
+  ]
+}
 
+resource "aws_batch_job_definition" "example" {
+  name = "t_this-job-definition"
+  type = "container"
+  container_properties = <<CONTAINER_PROPERTIES
+{
+    "command": ["echo", "Hello World"],
+    "image": "busybox",
+    "memory": 120,
+    "vcpus": 1,
+    "environment": [
+        {"name": "EXAMPLE_KEY", "value": "EXAMPLE_VALUE"}
+    ]
+}
+CONTAINER_PROPERTIES
+}
+
+
+resource "aws_batch_job_definition" "test" {
+  name = "t_this-job-definition2"
+  type = "container"
+
+  container_properties = <<CONTAINER_PROPERTIES
+{
+    "command": ["ls", "-la"],
+    "image": "busybox",
+    "memory": 120,
+    "vcpus": 1,
+    "volumes": [
+      {
+        "host": {
+          "sourcePath": "/tmp"
+        },
+        "name": "tmp"
+      }
+    ],
+    "environment": [
+        {"name": "VARNAME", "value": "VARVAL"}
+    ],
+    "mountPoints": [
+        {
+          "sourceVolume": "tmp",
+          "containerPath": "/tmp",
+          "readOnly": false
+        }
+    ],
+    "ulimits": [
+      {
+        "hardLimit": 1024,
+        "name": "nofile",
+        "softLimit": 1024
+      }
+    ]
+}
+CONTAINER_PROPERTIES
+}
