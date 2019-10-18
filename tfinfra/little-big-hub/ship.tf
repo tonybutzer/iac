3207191daad6458a936b803197525806d7ffcb5e
tonybutzer <tonybutzer@gmail.com>
2019-10-18 16:26:08 +0000

    automatic git update from Makefile

--- tfinfra/little-big-hub/ship.tf
+++ tfinfra/little-big-hub/ship.tf
@@ -10,7 +10,7 @@ provider "aws" {
 */
 
 
-resource "aws_instance" "mini1" {
+resource "aws_instance" "hub1" {
 
   ami           = "${var.ami}"
 
@@ -18,13 +18,13 @@ resource "aws_instance" "mini1" {
   key_name                    = "${var.key_name}"
   subnet_id                   = "${var.subnet_id}"
   tags = {
-    Name = "butzer-elastic1"
+    Name = "butzer-little-big-hub1"
     Owner = "butzer@contractor.usgs.gov"
     Project = "LPIP"
   }
   iam_instance_profile                    ="${var.iam_role}"
   # security_groups = ["${var.security_group_ssh}", "${var.security_group_ping}", "${aws_security_group.xrdp.id}"]
   security_groups = ["${var.security_group_ssh}", "${var.security_group_ping}", "${var.security_group_web}"]
-  root_block_device {volume_size = 30}
+  root_block_device {volume_size = 120}
   user_data                   = "${file("files/os_boot.sh")}"
 }
